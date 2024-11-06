4fe1db421fdba99099fb68361275542d8c99645a
ekoleci <enco6814@gmail.com>
2024-11-06 00:40:13 +0000

    completed

---
+++ lab5c.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Author ID: ekoleci
+
+def add(number1, number2):
+    try:
+        return number1 + number2
+    except TypeError:
+        return 'error: could not add numbers'
+
+def read_file(filename):
+    try:
+        with open(filename, 'r') as f:
+            return f.readlines()
+    except FileNotFoundError:
+        return 'error: could not read file'
+
+if __name__ == '__main__':
+    print(add(10, 5))
+    print(add('10', 5))
+    print(add('abc', 5))
+    print(read_file('seneca2.txt'))
+    print(read_file('file10000.txt'))
